dce03e486c55349a82f315c717d2db59196c5ab5
Adrian Cabrera <59811924+AdrianC47@users.noreply.github.com>
2023-09-25 22:03:09 -0800

    Columnas y Estilos

--- Proyectos-Flutter/contador/lib/src/pages/home_page.dart
+++ Proyectos-Flutter/contador/lib/src/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
 
+  final estiloTexto = new TextStyle(fontSize: 25);
+
   @override
   Widget build(BuildContext context) {
  
@@ -11,7 +13,14 @@ class HomePage extends StatelessWidget{
         centerTitle: true,
       ) , 
         body: Center(
-          child: Text("Hola Adrian"),
+          child: Column(
+            mainAxisAlignment:MainAxisAlignment.center ,
+            children: <Widget>[
+              Text('Número de clicks:',style: estiloTexto,),
+              Text('0',style: estiloTexto,)
+            ],
+            )
+
         ),
     ); //
   }
