bb1926c9395b2b6b0307121145a3335a0f736c9a
Adrian Cabrera <59811924+AdrianC47@users.noreply.github.com>
2023-10-02 15:45:25 -0800

    Crear ListTile a partir de una lista estática

--- Proyectos-Flutter/componentes/lib/src/pages/home_temp.dart
+++ Proyectos-Flutter/componentes/lib/src/pages/home_temp.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
 
+  final opciones = ['Cadillac Solitario','Name','Minutos','Inevitable','16'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,20 +12,35 @@ class HomePageTemp extends StatelessWidget {
         title: Text("Componentes Temp"),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text("ListTitle Title"),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("ListTitle Title"),
-          ),
-          Divider(),
-          ListTile(
-            title: Text("ListTitle Title"),
-          )
-        ],
+        children: _crearItems()
       ),
     );
+  }
+
+
+  List<Widget> _crearItems(){
+    // Tenemos dos formas de transformar una lista en una lista de Widgets
+
+    List<Widget> lista =  [];// se crea una lista de Widgets
+
+    for (String opt in opciones) {
+
+        final tempWidget = ListTile(
+          title: Text(opt),
+          // Di
+        );
+        //Aqui se usa el operador de cascada 
+        lista..add(tempWidget) //Este operador permite realizar varias operaciones en el mismo objeto sin tener que repetir el nombre del objeto
+             ..add(Divider(color: Colors.purple,)); // esta es con codigo mas limpio, Continuando con la cascada, se está agregando otro widget a la lista.
+
+        // lista.add(Divider(color: Colors.purple,)); //esta es la forma tradicional
+    }
+
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta(){
+     
   }
 }
